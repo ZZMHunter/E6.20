3d3c9dce1c41d2e4f6e2cd41538da3b6d332b3fb
ZZMHunter <zhao.zhang50@myhunter.cuny.edu>
2025-12-11 12:05:21 -0500

    fix 4

--- submission.cpp
+++ submission.cpp
@@ -14,23 +14,14 @@ vector<int> merge_sorted(vector<int> a, vector<int> b){
     int indA = 0;
     int indB = 0;
     vector<int> x;
-    for (int i = 0; i < a.size()+b.size(); i++){
-        if (indA!=-1 && indB==-1||a[indA]<=b[indB]){
+    for (int i = 0; i < a.size() + b.size(); i++){
+        if ((indA < a.size()) && (indB >= b.size() || a[indA] <= b[indB])) {
             x.push_back(a[indA]);
-            if (indA != -1){
-                indA++;
-            }
-            if (indA == a.size()){
-                indA = -1;
-            }
-        }else{
+            indA++;
+        } 
+        else {
             x.push_back(b[indB]);
-            if (indB != -1){
-                indB++;
-            }
-            if (indB == b.size()){
-                indB = -1;
-            }
+            indB++;
         }
     }
     return x;
